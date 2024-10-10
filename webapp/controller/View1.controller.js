e60380b33cf398d91b4b44c082b719410a1343c6
Nataliia Kovalchuk <mag.nata.isterium@gmail.com>
2024-10-10 09:23:35 +0000

    edit alert button text

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -9,7 +9,7 @@ sap.ui.define([
         onInit: function () {
         },
         onPress: function () {
-            MessageBox.alert("You have been alerted!");
+            MessageBox.alert("haha!");
         }
     });
 });
